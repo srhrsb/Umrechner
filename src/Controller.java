a4654afa32c91867fa408f0f16c0d6bf3ca07830
srhrsb <sebastian.boettcher2@srh.de>
2024-05-28 15:58:01 +0200

    Initial commit

---
+++ src/Controller.java
@@ -0,0 +1,52 @@
+import java.awt.event.ActionEvent;
+import java.util.Arrays;
+
+public class Controller {
+
+    View view;
+
+    public Controller() {
+        view = new View( 500, 170);
+        view.addActionButtonHandler(this::calculate);
+    }
+
+    public static void main(String[] args) {
+        new Controller();
+    }
+
+    public void calculate( ActionEvent event ){
+
+       // Logik für Berechnung
+
+        //ToDo: string mit Aufgabe holen
+        String task = view.getTaskText();
+
+        //ToDo: Parsen des strings
+        var taskData = parseTaskInput( task );
+        System.out.println(Arrays.toString(taskData));
+
+        //ToDo: Berechnung/Umrechnung
+
+
+
+        //ToDo: Ergebnis ausgeben
+
+    }
+
+    /**
+     * Parsen der Nutzereingabe für die Berechnung
+     * @param task Nutzereingabe
+     * @return Daten
+     */
+    public String[] parseTaskInput( String task){
+        var tempTaskData = task.split(" ");
+        String[] taskData = {tempTaskData[0], tempTaskData[1], tempTaskData[3] };
+        return taskData;
+    }
+
+
+
+
+
+
+}
---
+++ src/View.java
@@ -0,0 +1,66 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class View extends JFrame {
+
+    private JButton actionBtn;
+    private JTextField taskTf;
+    private JLabel resultLabel;
+
+
+    public View( int width, int height ){
+         setSize( width, height);
+         setTitle("Umrechner");
+         setDefaultCloseOperation( DISPOSE_ON_CLOSE );
+         addUIComponents();
+         setVisible(true);
+    }
+
+
+    /**
+     * Fügt UI Elemente dem Fenster hinzu
+     */
+    private void addUIComponents(){
+        //Layout setzen
+        setLayout(new BorderLayout());
+
+        //Panel erstellen
+        JPanel topPanel = new JPanel();
+        JPanel centerPanel = new JPanel();
+        JPanel bottomPanel = new JPanel();
+
+        centerPanel.setLayout(new GridLayout( 2, 1));
+        centerPanel.setBorder(new EmptyBorder(5,5,5,5));
+
+
+        add( topPanel, BorderLayout.NORTH);
+        add( centerPanel, BorderLayout.CENTER);
+        add(bottomPanel, BorderLayout.SOUTH);
+
+        actionBtn = new JButton("Umrechnen");
+
+        JLabel headline = new JLabel("" +
+                "Bitten geben Sie die Aufgabe nach folgendem Muster ein:" +
+                "zb: 2 cm in m");
+
+        taskTf = new JTextField();
+
+        resultLabel = new JLabel("<Hier ist später die Lösung>");
+
+        topPanel.add(headline);
+        centerPanel.add(taskTf);
+        centerPanel.add(resultLabel);
+        bottomPanel.add(actionBtn);
+    }
+
+    public void addActionButtonHandler( ActionListener listener){
+        actionBtn.addActionListener( listener );
+    }
+
+    public String getTaskText(){
+       return taskTf.getText();
+    }
+
+}
